c3ab14c3f3b6f4ae699b61068697cf50a363bfc0
KelliBre <Kelli.Brecunier@gmail.com>
2022-09-16 16:39:31 +1000

    Fixed notes for better explaination of while loop

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 //Introductio:Programing is the act of constructing a program--a set of instructions telling a computer what to do! 
 //declares varibles
 let total = 0, count = 1;
-//while value of count is less 10 = true 
+//while loop will run as long as the condition given hold true.
 while(count <= 10){ 
+    //total becomes sum of range of 1-10 
     total += count;
-    count += 1;
-    //total value increased by 1 total + count looped   || 1 +2 + 3 + 4 + 5 + 6  + 7 + 8 + 9 + 10 = 55
+    count += 1; 
+
 
 }
-console.log(count);//55
+console.log(total);
+
+
+
+
 
 
 
