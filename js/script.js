969ac7bac098d0505d8ee9a088331a77d275e72b
KelliBre <Kelli.Brecunier@gmail.com>
2022-09-16 16:11:04 +1000

    fixed file structure

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Introductio:Programing is the act of constructing a program--a set of instructions telling a computer what to do! 
+let total = 0, count = 1;
+while(count <= 10){
+    total += count;
+    count += 1;
+
+}
+
+console.log(total);
+
