535fa307f2640e6ab085b744f94aa3cc1bd5cfad
KelliBre <Kelli.Brecunier@gmail.com>
2022-09-16 16:30:05 +1000

    Added code from into allow with notes.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 //Introductio:Programing is the act of constructing a program--a set of instructions telling a computer what to do! 
+//declares varibles
 let total = 0, count = 1;
-while(count <= 10){
+//while value of count is less 10 = true 
+while(count <= 10){ 
     total += count;
     count += 1;
+    //total value increased by 1 total + count looped   || 1 +2 + 3 + 4 + 5 + 6  + 7 + 8 + 9 + 10 = 55
 
 }
+console.log(count);//55
 
-console.log(total);
+
+
 
